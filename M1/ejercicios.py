43fd9c9172c0094c43b6c37d96372bfeb24350eb
Pablo Peralta <pablo_peralta_corebiz@whirlpool.com>
2026-02-07 15:57:08 -0300

    upload first classes

---
+++ M1/ejercicios.py
@@ -0,0 +1,132 @@
+# lado1=input("Ingrese la medida del lado del cuadrado:")
+# lado=int(lado1)
+# superficie=lado*lado
+# print("La superficie del cuadrado es")
+# print(superficie)
+
+# -------------------------------------------------------
+
+# Programa con un error sintáctico:
+
+# lado=int(input("Ingrese la medida del lado del cuadrado:"))
+# superficie=lado*lado
+# print("La superficie del cuadrado es")
+# print(Superficie)
+
+# ----------------------------------------------------------
+
+# Programa con un error lógico:
+
+# lado=int(input("Ingrese la medida del lado del cuadrado:"))
+# superficie=lado*lado*lado
+# print("La superficie del cuadrado es")
+# print(superficie)
+
+# ----------------------------------------------------------
+
+# num1=int(input("ingrese primer valor:"))
+# num2=int(input("ingrese segundo valor:"))
+# suma=num1+num2
+# producto=num1*num2
+# print("La suma de los dos valores es")
+# print(suma)
+# print("El producto de los dos valores es")
+# print(producto)
+
+# --------------------------------------------------------------
+
+# precio=int(input("Ingrese el precio del producto:"))
+# cantidad=int(input("Ingrese la cantidad de productos a llevar:"))
+# importe=precio*cantidad
+# print("El importe a pagar es")
+# print(importe)
+
+# ----------------------------------------------------------------
+
+sueldo=int(input("Ingrese cual es su sueldo:"))
+if sueldo>3000:
+    print("Esta persona debe abonar impuestos")
+
+# -----------------------------------------------------------------
+
+# num1=int(input("Ingrese primer valor:"))
+# num2=int(input("ingrese segundo valor:"))
+# print("El valor mayor es")
+# if num1>num2:
+#     print(num1)
+# else:
+#     print(num2)
+
+# --------------------------------------------------------------
+
+# Operadores Relacionales:
+
+# == Igualdad
+
+# != Desigualdad
+
+# < menor
+
+# <= menor o igual
+
+# > mayor
+
+# >= mayor o igual
+
+# Operadores Matemáticos
+
+# + suma
+# - resta
+# * multiplicación
+# / división de flotantes
+# // división de enteros
+# % resto de una división
+# ** exponenciación
+
+# ---------------------------------------------------------------
+
+# nota1=int(input("Ingrese primer nota:"))
+# nota2=int(input("Ingrese segunda nota:"))
+# nota3=int(input("Ingrese tercer nota:"))
+# prom=(nota1+nota2+nota3)/3
+# if prom>=7:
+#     print("Promocionado")
+# else:
+#     if prom>=4:
+#         print("Regular")
+#     else:
+#         print("Reprobado")
+
+# --------------------------------------------------------------
+
+# num1=int(input("Ingrese primer valor:"))
+# num2=int(input("Ingrese segundo valor:"))
+# num3=int(input("Ingrese tercer valor:"))
+# print("El mayor de los tres valores es")
+# if num1>num2 and num1>num3:
+#     print(num1)
+# else:
+#     if num2>num3:
+#         print(num2)
+#     else:
+#         print(num3)
+
+# -------------------------------------------------------------
+
+# dia=int(input("Ingrese nro de día:"))
+# mes=int(input("Ingrese nro de mes:"))
+# año=int(input("Ingrese nro de año:"))
+# if mes==1 or mes==2 or mes==3:
+#     print("Corresponde al primer trimestre")
+
+# --------------------------------------------------------------
+
+# ejercicio
+
+# dd=int(input("Ingrese nro de día:"))
+# mm=int(input("Ingrese nro de mes:"))
+# aa=int(input("Ingrese nro de año:"))
+# if mm==12 and dd==25:
+#     print("La fecha ingresada corresponde a navidad.")
+
+# ---------------------------------------------------------------
---
+++ M1/if.py
@@ -0,0 +1,15 @@
+user = input("Ingresar usuario: ")
+password = input("Ingresar contraseña: ")
+
+if user == "PaulPeraltta":
+    if password == "1234":
+        print("Bienvenido "+user)
+    else:
+        print("Contraseña incorrecta")
+else:
+    print("Usuario incorrecto")
+
+
+
+
+
