5abbf64b210af4403ce3d0499b27a06fc5a0cb69
PaulPeraltta <paulperaltta@gmail.com>
2026-08-28 18:17:56 -0300

    class3 finished

---
+++ M1/Clase 3/while.py
@@ -0,0 +1,13 @@
+x=1
+while x<100:
+    print("el valor es menor a 100, es:", x)
+    x+=1
+print("termino el bucle while")
+
+##otro ejemplo
+numero_final= int(input("Ingresa el numero final:"))
+x=0
+while x<numero_final:
+    print("Aun no llegamos")
+    x=x+1
+print("ya si", x)
